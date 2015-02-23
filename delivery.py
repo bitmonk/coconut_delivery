354f25a7a4556e75023d64df00265c2bb4aff939
Justin Alan Ryan <Justin_Ryan@gap.com>
2015-02-23 15:29:57 -0800

    try calculating the jet streams, something still isn't right.

--- delivery.py
+++ delivery.py
@@ -28,3 +28,21 @@ for line in pathfile:
 logger.info("Constant Energy required to fly one mile: %s" % const_energy)
 logger.info("Jet Streams: %s" % jet_streams)
 
+position = 0
+old_position = None
+energy_consumed = 0
+for stream in jet_streams:
+  start = stream[0]
+  end = stream[1]
+  energy_required = stream[2]
+  if start > position:
+    old_position = position
+    position = end
+    energy_consumed += energy_required
+    logger.info("Moved from %s to %s using %s energy" % (old_position, position, energy_required))
+  else:
+    logger.info("Did not move from %s to %s using %s energy" % (position, end, energy_required))
+
+logger.info("Moved %s positions using %s total energy" % (position, energy_consumed))
+
+
